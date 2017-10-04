d31fca86494cd57d4c241d3ff6ea44047fb0b53b
Shalvah Adebayo <shalvah.adebayo@konga.com>
2017-10-04 09:56:27 +0100

    Add endpoint for Likes/Unlikes

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,15 @@ router.get('/', (req, res, next) => {
     Post.find().exec((err, posts) => {
         res.render('index', { posts: posts });
     });
-
 });
+
+router.post('/posts/:id/act', (req, res, next) => {
+    const action = req.body.action;
+    const counter = action === 'Like' ? 1 : -1;
+    Post.update({_id: req.params.id}, {$inc: {likes_count: counter}}, {}, (err, numberAffected) => {
+        res.send('');
+    });
+});
+
 
 module.exports = router;
