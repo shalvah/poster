dd7405195c3de2fa26c6e67b01f0f82e69cdc4f0
Shalvah Adebayo <shalvah.adebayo@konga.com>
2017-10-06 18:48:34 +0100

    Implement messaging with Pusher

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,17 @@ router.post('/posts/:id/act', (req, res, next) => {
     const action = req.body.action;
     const counter = action === 'Like' ? 1 : -1;
     Post.update({_id: req.params.id}, {$inc: {likes_count: counter}}, {}, (err, numberAffected) => {
+        let Pusher = require('pusher');
+
+        let pusher = new Pusher({
+            appId: process.env.PUSHER_APP_ID,
+            key: process.env.PUSHER_APP_KEY,
+            secret: process.env.PUSHER_APP_SECRET,
+            cluster: 'eu'
+        });
+
+        let payload = { action: action, postId: req.params.id };
+        pusher.trigger('post-events', 'postAction', payload, req.body.socketId);
         res.send('');
     });
 });
